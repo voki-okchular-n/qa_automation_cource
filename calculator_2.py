8984d9ba8e087a508012ecef455cc2b61bf21f85
Нина Окчулар <nina.okchular@vokigames.com>
2025-06-08 10:21:55 +0300

    Проверочное задание: Стандартные библиотеки Python - калькулятор

--- calculator_2.py
+++ calculator_2.py
@@ -1,4 +1,7 @@
+import math
+import random
 import re
+from collections import Counter
 
 from calculator_module import AdvancedCalc, ZeroError
 from calculator_module import Timer, factorial
@@ -58,6 +61,7 @@ with Timer():
     factorial(100)
 
 print("\nСравнение времени для факториала 1000:")
+
 print("Время для факториала 1000 первый раз без кэша:")
 with Timer():
     factorial(1000)
@@ -65,3 +69,19 @@ with Timer():
 print("Время для факториала 1000 с кэшем:")
 with Timer():
     factorial(1000)
+
+print("Время с использованием math.factorial:")
+with Timer():
+    math.factorial(1000)
+
+print("\nГенерируем 1000 случайных чисел от 1 до 10:")
+
+numbers = []
+for i in range(1000):
+    numbers.append(random.randint(1, 10))
+
+counts = Counter(numbers)
+
+print("Итоги генерации чисел:")
+for number, count in sorted(counts.items()):
+    print(f"Число {number}: выпало {count} раз")
